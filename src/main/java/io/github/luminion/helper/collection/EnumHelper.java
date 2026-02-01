1c4ed0a490b0f20504546e561911aa3dac379b9d
luminion <604647004@qq.com>
2026-02-01 16:31:36 +0800

    feat(enum-helper): 添加获取枚举键值对映射的功能
    - 引入Collectors工具类简化流操作
    - 新增getMap方法，通过valueGetter获取值映射为Map
    - 利用stream和toMap实现键值对映射转换
    - 方便用户获取枚举中key与value的映射关系

--- src/main/java/io/github/luminion/helper/collection/EnumHelper.java
+++ src/main/java/io/github/luminion/helper/collection/EnumHelper.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 枚举助手，方便获取枚举中的key和value
@@ -79,5 +80,17 @@ public class EnumHelper<E extends Enum<E>, K> {
     public <V> V getValue(K key, Function<E, V> valueGetter) {
         E e = resolve(key);
         return e == null ? null : valueGetter.apply(e);
+    }
+
+    /**
+     * 获取枚举键值对映射的Map
+     * 
+     * @param valueGetter 获取值的 getter
+     * @param <V>         值类型
+     * @return 映射的Map
+     */
+    public <V> Map<K, V> getMap(Function<E, V> valueGetter) {
+        return keyMap.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> valueGetter.apply(e.getValue())));
     }
 }
